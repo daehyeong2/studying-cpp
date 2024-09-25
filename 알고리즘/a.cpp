17c84eb814a5b0942b48013eb4a68127d5e36fb9
daehyeong2 <daehyeong2010@naver.com>
2024-09-26 02:48:43 +0900

    에라토스테네스의 체 (check 함수)

--- 알고리즘/a.cpp
+++ 알고리즘/a.cpp
@@ -1,21 +1,20 @@
 #include<bits/stdc++.h>
 using namespace std;
-const int max_n = 40;
-bool che[max_n + 1]; // 예를 들어 40을 넣었을 때 che[40]이 참조가 되므로 max_n + 1을 넣어야 함.
 
-// max_n까지의 소수를 만드는 함수
-vector<int> era(int mx_n){
-	vector<int> v;
-	for(int i = 2; i <= mx_n; i++){
-		if (che[i]) continue;
-		for(int j = 2*i; j <= mx_n; j += i){
-			che[j] = 1;
+bool check(int n) {
+	if (n <= 1) return 0;
+	if (n == 2) return 1;
+	if (n % 2 == 0) return 0;
+	for (int i = 3; i * i <= n; i++) {
+		if (n % i == 0) return 0;
+	}
+	return 1;
+}
+
+int main(){
+	for (int i = 1; i <= 100000; i++) {
+		if (check(i)) {
+			cout << i << "\n";
 		}
 	}
-	for(int i = 2; i <= mx_n; i++) if(che[i] == 0)v.push_back(i);
-	return v;
-}
-int main(){
-	vector<int> a = era(max_n);
-	for(int i : a) cout << i << " ";
 }
