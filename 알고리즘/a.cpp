7a20b416c51cd8314f03cb49e696dfd9e23fb29c
daehyeong2 <daehyeong2010@naver.com>
2024-09-26 05:24:20 +0900

    2차원 배열 회전

--- 알고리즘/a.cpp
+++ 알고리즘/a.cpp
@@ -1,18 +1,50 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int main() {
-	vector<int> v = { 1,2,3,4,5,6 };
+void rotate_left_90(vector<vector<int>>& key) {
+	int n = key.size();
+	int m = key[0].size();
+	vector<vector<int>> temp(m, vector<int>(n, 0));
+
+	for (int i = 0; i < m; i++) {
+		for (int j = 0; j < n; j++) {
+			temp[i][j] = key[j][m - i - 1];
+		}
+	}
+
+	key.resize(m);
+	key[0].resize(n);
 	
-	int i = 1;
-	int temp = v[i];
+	key = temp;
+	return;
+}
 
-	v[i] = v[i + 1];
-	v[i + 1] = v[i + 2];
-	v[i + 2] = v[i + 3];
-	v[i + 3] = temp;
-	for (int i : v) cout << i << " ";
-	cout << "\n";
+void rotate_right_90(vector<vector<int>>& key) {
+	int n = key.size();
+	int m = key[0].size();
+	vector<vector<int>> temp(m, vector<int>(n, 0));
 
+	for (int i = 0; i < m; i++) {
+		for (int j = 0; j < n; j++) {
+			temp[i][j] = key[n - j - 1][i];
+		}
+	}
+
+	key.resize(m);
+	key[0].resize(n);
+
+	key = temp;
+	return;
+}
+
+int main() {
+	vector<vector<int>> a = { {1,2,4}, {5,6,7}, {9,0,1} };
+	rotate_left_90(a);
+	for (int i = 0; i < a.size(); i++) {
+		for (int j = 0; j < a[0].size(); j++) {
+			cout << a[i][j] << " ";
+		}
+		cout << "\n";
+	}
 	return 0;
 }
